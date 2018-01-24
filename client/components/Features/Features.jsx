7ec025f2cd8016ecb7a18780cdccfbbb5559848e
mikeg <mikeg@steelhouse.com>
2018-01-23 20:12:39 -0800

    get started

--- client/components/Features/Features.jsx
+++ client/components/Features/Features.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-// import logo from '../../images/logo.png'
-
 const Features = () =>
   (
-    <div id="features">
+    <div id="features" className="section padding">
       <a name="features"></a>
-      <h2>Features</h2>
-      <h5>config 1</h5>
-      <li>
-        do this test
-        </li>
-      <h5>config 2</h5>
-      <li>
-        do this here
-        </li>
-      <h5>config 3</h5>
-      <li>
-        do this again
-        </li>
+      <div className="h2 fw-600 margin-top margin-bottom-m raleway">Features</div>
+      <div>
+        PeerConnect is a proof of concept that aims to serve static assets (videos/images)
+        over a peer to peer delivery network powered by WebRTC (images), WebTorrent (videos),
+        and WebSockets (signaling).
+      </div>
+      <div className="h5 fw-600 margin-top-lg margin-bottom-m raleway">Images</div>
+      <div>
+        PeerConnect uses WebRTC for image P2P transfers. By using websockets, we are able to
+        coordinate data-channel connections between two different peers. If no available peers
+        are present, images are loaded from the server. Once a peer finishes downloading, they
+        become an initiator for future P2P data transfers.
+      </div>
+      <div className="h5 fw-600 margin-top-lg margin-bottom-m raleway">Video</div>
+      <div>
+        PeerConnect uses WebTorrent and torrenting protocols for video P2P transfers. By utilizing
+        the server as a webseed for videos, as more and more individuals visit the site, video streams
+        will get progressively stronger and rely less on the initial webseed hosted on the server.
+      </div>
     </div>
   );
 
--- client/components/GetStarted/GetStarted.jsx
+++ client/components/GetStarted/GetStarted.jsx
@@ -1,63 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 
-// import logo from '../../images/logo.png'
+// Components
+import Instruction from './Instruction.jsx';
 
-const GetStarted = () =>
-  (
-    <div id="get-started">
-      <a name="get-started"></a>
-      <div id="instructions">
-        <h2>Get Started</h2>
-        <div className="instruction-bullet">
-          <div className="instruction-bullet-header">
-            <img className="instruction-icon" src="https://image.flaticon.com/icons/png/128/291/291201.png" />
-            <h6>Add this script to your front end.</h6>
-          </div>
-          <div className="instruction-bullet-content">
-            <li>
-              code snippet here
-            </li>
-            <li>"const PeerConnect = require 'peer-connect'"</li>
-          </div>
+// Assets
+import graphic from '../../images/pc-graphic.png'
+
+const instructions = [
+  {
+    title: 'Add this script to your front end.',
+    snippet1: '<script src="peer-connect.min.js"><script/>'
+  },
+  {
+    title: 'Change source tags of which assets you want',
+    snippet1: '<img data-src="my-awesome-cat-pic.jpg"/>'
+  },
+  {
+    title: 'Require PeerConnect to your back end.',
+    direction: 'npm install peer-connect in your project directory',
+    snippet1: 'const PeerConnect = require(\'peer-connect\')'
+  },
+  {
+    title: 'Invoke PeerConnect!',
+    direction: 'Define your config object and call the peerConnect function.',
+    snippet1: 'const config = { big ol\' placeholder config object}',
+    snippet2: 'const server = app.listen(3000)',
+    snippet3: 'peerConnect(config, server)'
+  }
+];
+
+class GetStarted extends Component {
+  constructor() {
+    super();
+  }
+
+  renderInstructions() {
+    return instructions.map((instruction, i) => {
+      return (
+        <Instruction
+          key={`instruction-${i}`}
+          title={instruction.title}
+          direction={instruction.direction}
+          snippet1={instruction.snippet1 ? instruction.snippet1 : null}
+          snippet2={instruction.snippet2 ? instruction.snippet2 : null}
+          snippet3={instruction.snippet3 ? instruction.snippet3 : null}
+        />
+      )
+    });
+  }
+
+  render() {
+    return (
+      <div id="get-started" className="section padding">
+        <a name="get-started"></a>
+        <div id="instructions">
+          <div className="h2 fw-600 margin-top margin-bottom-m raleway">Get Started</div>
+          {this.renderInstructions()}
         </div>
-        <div className="instruction-bullet">
-          <div className="instruction-bullet-header">
-            <img className="instruction-icon" src="https://image.flaticon.com/icons/png/128/291/291201.png" />
-            <h6>Change source tags of which assets you want </h6>
-          </div>
-          <div className="instruction-bullet-content">
-            <li>
-              code snippet here
-            </li>
-            <li>"const PeerConnect = require 'peer-connect'"</li>
-          </div>
-        </div>
-        <div className="instruction-bullet">
-          <div className="instruction-bullet-header">
-            <img className="instruction-icon" src="https://image.flaticon.com/icons/png/128/291/291201.png" />
-            <h6>Install PeerConnect</h6>
-          </div>
-          <div className="instruction-bullet-content">
-            <li>"npm install peer-connect" in your project directory</li>
-            <li>"const PeerConnect = require 'peer-connect'"</li>
-          </div>
-        </div>
-        <div className="instruction-bullet">
-          <div className="instruction-bullet-header">
-            <img className="instruction-icon" src="https://image.flaticon.com/icons/png/128/291/291201.png" />
-            <h6>Invoke PeerConnect!</h6>
-          </div>
-          <div className="instruction-bullet-content">
-            <li>define your config object</li>
-            <li>call the peerConnect function</li>
-            <li>peerConnect(config, server)</li>
-          </div>
+        <div className="center-content">
+          <img id="instruction-graphic" src={graphic} />
         </div>
       </div>
-      <img id="instruction-gif" src="https://media.giphy.com/media/B2vBunhgt9Pc4/giphy.gif" />
-    </div>
-  );
+    );
+  }
+}
 
 
 export default GetStarted;
